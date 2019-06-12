5dadadac3f2cb244c5081af30321c2153a9b5b45
kresnik87 <jeyser.aguilar@gmail.com>
2019-06-13 01:17:47 +0200

    add button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,67 @@
 import { Component,OnInit } from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
+const IMAGE_SIZE = 100;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements  OnInit {
-  title = 'classificator';
-  model:any;
-  ngOnInit(): void {
 
+export class AppComponent implements  OnInit {
+  title = 'Fruit Clasificator';
+  model: any;
+  predictions:any;
+  public imagePath;
+  imgURL: any;
+  public message: string;
+  ngOnInit(): void {
+ this.loadlModel();
   }
 
   async loadlModel(){
-    this.model= await tf.loadGraphModel('./assets/model/model.json');
+    this.model= await tf.loadLayersModel('./assets/model/model.json');
+    
+    console.log(this.model);
+  }
+
+  async predict(imageData: any) {
+    
+    
+    await tf.tidy(() => {
+      const img=tf.browser.fromPixels(imageData).toFloat;
+      const offset = tf.scalar(127.5);
+      // Normalize the image from [0, 255] to [-1, 1].
+      const normalized = img.sub(offset).div(offset);
+
+      // Reshape to a single-element batch so we can pass it to predict.
+       const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+      const output = this.model.predict(batched) as any;
+  
+      // Save predictions on the component
+      this.predictions = Array.from(output.dataSync()); 
+      console.log(this.predictions);
+    });
+  
+  }
+
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      console.log(reader);
+      this.imgURL = reader.result;
+      console.log(this.imgURL) 
+      this.predict(files);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatButtonModule,
+    MatCheckboxModule,
     BrowserModule
   ],
   providers: [],
